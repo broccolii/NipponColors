40c121a5646f70bac407060fa0ebf0e36a750cfa
broccolii <Broccoli@BroccolideMacBook-Pro.local>
2015-11-19 14:16:42 +0800

    Add comment

--- ColorPicker/Model/ColorInfo.swift
+++ ColorPicker/Model/ColorInfo.swift
@@ -22,55 +22,14 @@ class ColorInfo {
     var RValue: Int!
     var GValue: Int!
     var BValue: Int!
-    
-    init() {
-        colorEN = ""
-        colorName = ""
-        colorCount = ""
-        RBGHexValue = ""
-        
-        CValue = 0
-        MValue = 0
-        YValue = 0
-        KValue = 0
-        
-        RValue = 0
-        GValue = 0
-        BValue = 0
-    }
-    
-    class func randomInit() -> ColorInfo {
-        let info = ColorInfo()
-        
-        let colorENArr = ["MIZU", "ASAGI", "KARAKURENAI", "HANADA", "KON"]
-        info.colorEN = colorENArr[Int(arc4random() % 5)]
-        
-        let colorNameArr = ["苗", "芥子", "玉蜀黍", "肥後煤竹", "錆鉄御納戸"]
-        info.colorName = colorNameArr[Int(arc4random() % 5)]
-        
-        let colorCountArr = ["008", "088", "099", "222", "111"]
-        info.colorCount = colorCountArr[Int(arc4random() % 5)]
-        
-        let RBGHexValueArr = ["#B5495B", "#D05A6E", "#FEDFE1", "#D0104C", "#EEA9A9"]
-        info.RBGHexValue = RBGHexValueArr[Int(arc4random() % 5)]
-        
-        info.CValue = Int(arc4random() % 100)
-        info.MValue = Int(arc4random() % 100)
-        info.YValue = Int(arc4random() % 100)
-        info.KValue = Int(arc4random() % 100)
-        
-        info.RValue = Int(arc4random() % 256)
-        info.GValue = Int(arc4random() % 256)
-        info.BValue = Int(arc4random() % 256)
-
-        return info
-    }
-    
+  
     /**
-     "name": "燕脂",
-     "cmyk": "042093068006",
-     "rgb": "9F353A",
-     "color": "ENJI"
+     dictionary To model
+     
+     - parameter dic:   从本地文件中读取的数据
+     - parameter index: 编号
+     
+     - returns:  model
      */
     class func dicToModel(dic: [String: String], index: Int) -> ColorInfo {
         let model = ColorInfo()
@@ -93,39 +52,60 @@ class ColorInfo {
         return model
     }
     
+    /**
+     将 十六进制  转换成 255, 255, 255
+     
+     - parameter str: 十六进制字符串
+     
+     - returns:  RGB value
+     */
     class func formatRGBValue(str: String) -> (Int, Int, Int) {
         var red: UInt32 = 0
         var green: UInt32 = 0
         var blue: UInt32 = 0
         
-        let range1 = Range<String.Index>(start: str.startIndex.advancedBy(0) , end: str.startIndex.advancedBy(2))
-        NSScanner(string: str.substringWithRange(range1)).scanHexInt(&red)
+        let RRange = Range<String.Index>(start: str.startIndex.advancedBy(0) , end: str.startIndex.advancedBy(2))
+        NSScanner(string: str.substringWithRange(RRange)).scanHexInt(&red)
         
-        let range2 = Range<String.Index>(start: str.startIndex.advancedBy(2) , end: str.startIndex.advancedBy(4))
-        NSScanner(string: str.substringWithRange(range2)).scanHexInt(&green)
+        let GRange = Range<String.Index>(start: str.startIndex.advancedBy(2) , end: str.startIndex.advancedBy(4))
+        NSScanner(string: str.substringWithRange(GRange)).scanHexInt(&green)
         
-        let range3 = Range<String.Index>(start: str.startIndex.advancedBy(4) , end: str.startIndex.advancedBy(6))
-        NSScanner(string: str.substringWithRange(range3)).scanHexInt(&blue)
+        let BRange = Range<String.Index>(start: str.startIndex.advancedBy(4) , end: str.startIndex.advancedBy(6))
+        NSScanner(string: str.substringWithRange(BRange)).scanHexInt(&blue)
         
         return (Int(red), Int(green), Int(blue))
     }
     
+    /**
+     分割字符串
+     
+     - parameter str:  CMYK 字符串
+     
+     - returns:  CMYK 四个 值
+     */
     class func formatCMYKValue(str: String) -> (Int, Int, Int, Int) {
-        let range1 = Range<String.Index>(start: str.startIndex.advancedBy(0) , end: str.startIndex.advancedBy(3))
-        let int1 = Int(str.substringWithRange(range1))
+        let CRange = Range<String.Index>(start: str.startIndex.advancedBy(0) , end: str.startIndex.advancedBy(3))
+        let CValue = Int(str.substringWithRange(CRange))!
         
-        let range2 = Range<String.Index>(start: str.startIndex.advancedBy(3) , end: str.startIndex.advancedBy(6))
-        let int2 = Int(str.substringWithRange(range2))
+        let MRange = Range<String.Index>(start: str.startIndex.advancedBy(3) , end: str.startIndex.advancedBy(6))
+        let MValue = Int(str.substringWithRange(MRange))!
         
-        let range3 = Range<String.Index>(start: str.startIndex.advancedBy(6) , end: str.startIndex.advancedBy(9))
-        let int3 = Int(str.substringWithRange(range3))
+        let YRange = Range<String.Index>(start: str.startIndex.advancedBy(6) , end: str.startIndex.advancedBy(9))
+        let YValue = Int(str.substringWithRange(YRange))!
         
-        let range4 = Range<String.Index>(start: str.startIndex.advancedBy(9) , end: str.startIndex.advancedBy(12))
-        let int4 = Int(str.substringWithRange(range4))
+        let KRange = Range<String.Index>(start: str.startIndex.advancedBy(9) , end: str.startIndex.advancedBy(12))
+        let KValue = Int(str.substringWithRange(KRange))!
         
-        return (int1!, int2!, int3!, int4!)
+        return (CValue, MValue, YValue, KValue)
     }
     
+    /**
+     位数不足 用零补齐
+     
+     - parameter count: 编号
+     
+     - returns: 转换后的字符串
+     */
     class func formatColorCount(count: Int) -> String {
        return String(format: "%03d", count + 1)
     }
